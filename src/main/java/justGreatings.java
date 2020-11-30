bdd7c1fb6c6ee732a34cf27d36ef79609fcf1749
Sashk52 <a.tsven@gmail.com>
2020-11-30 12:48:59 +0200

    added Hello mates greating

---
+++ src/main/java/justGreatings.java
@@ -0,0 +1,5 @@
+public class justGreatings {
+    public static void main(String [] args){
+        System.out.println("Hello mates!");
+    }
+}
--- src/main/java/simpleHelloWoldApp.java
+++ src/main/java/simpleHelloWoldApp.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class simpleHelloWoldApp {
+public class simpleHelloWoldApp {
+    public static void main(String [] args){
+    System.out.println("Hello world!");
+    }
 }
